0bd2431fc3d0cdae186a5aa94487e3382bcf739d
Esh9999 <eshkin.bayramov@gmail.com>
2026-01-26 13:39:04 +0300

    Update test_arxiv_parser.py

--- fetcher-service/tests/test_arxiv_parser.py
+++ fetcher-service/tests/test_arxiv_parser.py
@@ -1,5 +1,4 @@
-from app import parse_arxiv_atom
-
+from fetcher_service_app import parse_arxiv_atom  # see note below
 
 SAMPLE_XML = """<?xml version="1.0" encoding="UTF-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
